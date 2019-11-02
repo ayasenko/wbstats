0027db17267fac4fb99e88b8098bcefe842b3502
Artem Yasenko <artem_yasenko@epam.com>
2019-11-02 15:44:21 +0200

    responsive styles added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import Table from './components/Table';
 import Form from './components/Form';
-import './style.scss';
+import './scss/style.scss';
 
 class App extends Component {
   render() {
